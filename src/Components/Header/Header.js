a2b9335cd2a3f1d73e7a3125bd301d4702162e4d
NitsNitika <nitsnitika0208@gmail.com>
2026-05-03 12:17:52 +0530

    fixed header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,11 @@
-import "./Header.css"
+import "./Header.css";
 
-const Header = () =>{
-    return <span onClick={() => window.scroll(0,0)} className='header'>Cinema & Chill</span>
-}
+const Header = () => {
+  return (
+    <span onClick={() => window.scroll(0, 0)} className="header">
+      Movie & Chill
+    </span>
+  );
+};
 
 export default Header;
